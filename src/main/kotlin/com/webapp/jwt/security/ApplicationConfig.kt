d0453b13d363737aa6afd6666f761e9632052656
D3xt3rr <mathieu.thibeault-thuot@outlook.com>
2023-03-20 19:45:41 -0400

    add railway mongodb

--- src/main/kotlin/com/webapp/jwt/security/ApplicationConfig.kt
+++ src/main/kotlin/com/webapp/jwt/security/ApplicationConfig.kt
@@ -9,7 +9,6 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
-import org.springframework.security.crypto.password.PasswordEncoder
 
 
 @Configuration
@@ -30,8 +29,6 @@ class ApplicationConfig(private val service : UserDetailServiceImpl) {
 
     @Bean
     fun passwordEncoder() : BCryptPasswordEncoder {
-        return BCryptPasswordEncoder(16)
+        return BCryptPasswordEncoder(10)
     }
-
-
 }
