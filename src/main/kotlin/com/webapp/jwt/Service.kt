b5ee7a8b21d517cf421e454b3f4f986b58da4e18
D3xt3rr <mathieu.thibeault-thuot@outlook.com>
2023-03-23 21:12:42 -0400

    add railway mongodb

--- src/main/kotlin/com/webapp/jwt/Service.kt
+++ src/main/kotlin/com/webapp/jwt/Service.kt
@@ -1,11 +1,7 @@
 package com.webapp.jwt
 
-import org.bson.conversions.Bson
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
-import org.springframework.security.core.userdetails.UserDetails
-import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.stereotype.Service;
 
@@ -24,7 +20,6 @@ class Service(
     }
 
     fun login(user : User) {
-
         auth
             .authenticate(
                 UsernamePasswordAuthenticationToken(
